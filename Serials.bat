2185f40c743b818f03e77965f7e89faedb96f39f
carsxndev <suatemirboke@gmail.com>
2025-12-15 23:31:23 +0300

    Add files via upload

---
+++ Serials.bat
@@ -0,0 +1,31 @@
+:a
+echo off
+cls
+color b
+
+echo Diskdrive:
+wmic diskdrive get serialnumber
+
+echo Bios:
+wmic bios get serialnumber
+
+echo Hwid:
+wmic csproduct get uuid
+
+echo Baseboard:
+wmic baseboard get serialnumber
+
+echo Memorychip:
+wmic memorychip get serialnumber
+
+echo MAC:
+getmac /v
+
+echo Press any key to check serials again
+echo carsxn#1337 for contact
+pause
+
+goto a
+
+
+
